5d857ac2e90a171e14f5465bb99faf6914f7be28
sawada <snp.inc.info@gmail.com>
2020-12-08 16:45:03 +0900

    nontan

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,72 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!,only:[:new,:create]
+  
+  # GET /blogs
+  # GET /blogs.json
+  def index
+    @blogs = Blog.all
+  end
+
+  # GET /blogs/1
+  # GET /blogs/1.json
+  def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
+  end
+
+  # GET /blogs/new
+  def new
+    @blog = Blog.new
+  end
+
+  # GET /blogs/1/edit
+  def edit
+  end
+
+  # POST /blogs
+  # POST /blogs.json
+  def create
+    @blog = current_user.blogs.build(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice:"ブログを作成しました"
+    else
+      render 'new'
+    end
+
+  end
+
+  # PATCH/PUT /blogs/1
+  # PATCH/PUT /blogs/1.json
+  def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  def destroy
+    @blog.destroy
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Blog.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def blog_params
+      params.require(:blog).permit(:title, :content)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  resources :blogs
+  resources  :favorites, only:[:create,:destroy]
+  root "blogs#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+end
